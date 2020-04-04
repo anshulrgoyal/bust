ffba9725963d274b21dfd6db281298c9cb0dafe0
Anshul Goyal <anshulgoel151999@gmail.com>
2020-04-05 04:01:10 +0530

    Fixed Port and Http

--- src/main.rs
+++ src/main.rs
@@ -234,11 +234,13 @@ async fn main() -> anyhow::Result<()> {
         None => return Err(anyhow::anyhow!("Error while making dns query")),
     };
     let lookup_time = lookup.elapsed().as_millis();
-    let socket = match schema {
+    let socket = match &req.uri().port() {
+        Some(port)=>SocketAddr::new(ip, port.as_u16()),
+        None=>match schema {
         "https" => SocketAddr::new(ip, 443),
         "http" => SocketAddr::new(ip, 80),
         _ => return Err(anyhow::anyhow!("Error while creating ip")),
-    };
+    }};
     let mut ac = Stats {
         connect: 0,
         handshake: 0,
@@ -273,8 +275,30 @@ async fn main() -> anyhow::Result<()> {
                 for _ in 1..args.concurrency {
                     v.push(make_http_request(&socket, body.as_slice()))
                 }
-                let data = futures::future::try_join_all(v).await?;
-                println!("{:?}", data);
+                let s = futures::future::try_join_all(v).await?;
+                s.iter().for_each(|c| {
+                    compeleted.push(c.compelete);
+                    min.connect = std::cmp::min(min.connect, c.connect);
+                    min.handshake = std::cmp::min(min.handshake, c.handshake);
+                    min.waiting = std::cmp::min(min.waiting, c.waiting);
+                    min.writing = std::cmp::min(min.writing, c.writing);
+                    min.read = std::cmp::min(min.read, c.read);
+                    min.compelete = std::cmp::min(min.compelete, c.compelete);
+
+                    max.connect = std::cmp::max(max.connect, c.connect);
+                    max.handshake = std::cmp::max(max.handshake, c.handshake);
+                    max.waiting = std::cmp::max(max.waiting, c.waiting);
+                    max.writing = std::cmp::max(max.writing, c.writing);
+                    max.read = std::cmp::max(max.read, c.read);
+                    max.compelete = std::cmp::max(max.compelete, c.compelete);
+
+                    ac.connect = ac.connect.add(c.connect);
+                    ac.handshake = ac.handshake.add(c.handshake);
+                    ac.waiting = ac.waiting.add(c.waiting);
+                    ac.writing = ac.writing.add(c.writing);
+                    ac.read = ac.read.add(c.read);
+                    ac.compelete = ac.compelete.add(c.compelete);
+                });
             }
             "https" => {
                 let mut v = Vec::with_capacity(args.concurrency as usize);
